2e67e858a0b42a2ec73ce27e9abfbaca52b3c480
slicycode <deparisjules180@gmail.com>
2022-01-04 23:31:01 +0100

    i wanna die

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async session({ session, token }) {
+    async session({ session, token, user }) {
       session.user.tag = session.user.name
         .split(' ')
         .join('')
