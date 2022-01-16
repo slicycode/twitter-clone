d8b9ed4b0a15f14b9e52fc15356da8b1ce3a43e3
slicycode <deparisjules180@gmail.com>
2022-01-16 16:40:15 +0100

    just perfectionist things

--- components/Post.js
+++ components/Post.js
@@ -111,7 +111,7 @@ function Post({ id, post, postPage }) {
               >
                 @{post?.tag}
               </span>
-            </div>
+            </div>{' '}
             ·{' '}
             <span className="hover:underline text-sm sm:text-[15px]">
               <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
